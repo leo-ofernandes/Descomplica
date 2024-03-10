c5dba14e9f748ebd59c71f573bec5c9bff6d8496
Leonardo <138024184+leo-ofernandes@users.noreply.github.com>
2024-03-10 15:13:50 -0300

    Aula 9

---
+++ desenvolvimentoDinamico/exemplo2/objetos.js
@@ -0,0 +1,34 @@
+// forma de criar objetos
+
+const pessoa = {
+  nome: "Lucas",
+  sobrenome: "Silva",
+};
+
+console.log(pessoa["sobrenome"]);
+
+// 2 - forma de criar objetos
+const funcionario = new Object();
+funcionario.nome = "Karen";
+funcionario.sobrenome = "Joana";
+
+console.log(funcionario.nome);
+
+// 3 - forma de criar objetos
+function criarPessoa(nome, sobrenome, i) {
+  return {
+    nome,
+    sobrenome,
+    idade: i,
+    nomeCompleto() {
+      return `${this.nome} ${this.sobrenome}`;
+    },
+  };
+}
+
+const p1 = criarPessoa("Arthur", "Silva", 20);
+
+const p2 = criarPessoa("Joao", "Silva", 34);
+
+console.log(p1.nomeCompleto());
+console.log(p2);
